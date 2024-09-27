7daa47d751a718a9ae39cee167462313bb79f950
Zumayaa <abzu_21@alu.uabcs.mx>
2024-09-27 00:44:24 -0700

    ya se ven las series mi apa

--- zumacinevue/src/components/SeriesAPI.vue
+++ zumacinevue/src/components/SeriesAPI.vue
@@ -1,21 +1,21 @@
 <template>
     <div>
-      <h2>Movies</h2>
+      <h2>Series</h2>
       <div v-if="error">{{ error }}</div>
       <div v-else-if="loading">Loading...</div>
       <div v-else>
-        <div v-for="movie in movies" :key="movie.id">
-          <h3>{{ movie.title }}</h3>
-          <p>{{ movie.overview }}</p>
+        <div v-for="serie in series" :key="serie.id">
+          <h3>{{ serie.title }}</h3>
+          <p>{{ serie.overview }}</p>
           <img
-            v-if="movie.poster_path"
-            :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
-            :alt="movie.title"
-            class="movie-poster"
+            v-if="serie.poster_path"
+            :src="`https://image.tmdb.org/t/p/w500${serie.poster_path}`"
+            :alt="serie.title"
+            class="serie-poster"
           />
-          <router-link :to="`/movie/${movie.id}`">VER SERIES</router-link>
+          <router-link :to="`/serie/${serie.id}`">VER SERIES</router-link>
         </div>
-        <button @click="loadMoreMovies" v-if="!loadingMore">Cargar mas peliculas</button>
+        <button @click="loadMoreSeries" v-if="!loadingMore">Cargar mas peliculas</button>
         <div v-if="loadingMore">Cargando mas peliculas...</div>
       </div>
     </div>
@@ -25,10 +25,10 @@
   import axios from 'axios';
   
   export default {
-    name: 'MoviesAPI',
+    name: 'SeriesAPI',
     data() {
       return {
-        movies: [],
+        series: [],
         loading: true,
         loadingMore: false, 
         error: null,
@@ -37,21 +37,21 @@
     },
   
     async mounted() {
-      await this.fetchMovies(this.currentPage);
+      await this.fetchSeries(this.currentPage);
     },
   
     methods: {
-      async fetchMovies(page) {
+      async fetchSeries(page) {
         try {
-          const response = await axios.get('https://api.themoviedb.org/3/discover/movie', {
+          const response = await axios.get('https://api.themoviedb.org/3/discover/tv', {
             params: {
               api_key: 'b27d7edb3072175fb8681650517059f7',
               page: page,
             },
           });
-          this.movies.push(...response.data.results);
+          this.series.push(...response.data.results);
         } catch (error) {
-          this.error = 'fallo en cargar mas peliculas, intente denuevo';
+          this.error = 'fallo en cargar mas series, intente denuevo';
           console.error(error);
         } finally {
           this.loading = false;
@@ -59,18 +59,18 @@
         }
       },
   
-      // cargar mas peliculas y no sobrecargar la api de golpe
-      async loadMoreMovies() {
+      // cargar mas series y no sobrecargar la api de golpe
+      async loadMoreSeries() {
         this.loadingMore = true;
         this.currentPage += 1;
-        await this.fetchMovies(this.currentPage);
+        await this.fetchSeries(this.currentPage);
       },
     },
   };
   </script>
   
   <style scoped>
-  .movie-poster {
+  .serie-poster {
     width: 150px;
     height: auto;
     margin-top: 10px;
