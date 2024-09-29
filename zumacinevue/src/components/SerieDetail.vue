c99a0adef7c8672fad4b8f17407bac21cf2647dc
Zumayaa <abzu_21@alu.uabcs.mx>
2024-09-29 15:07:15 -0700

    se agregó cast series

--- zumacinevue/src/components/SerieDetail.vue
+++ zumacinevue/src/components/SerieDetail.vue
@@ -11,7 +11,7 @@
       <p>{{ serie.overview }}</p>
 
       <div v-if="serie.genres.length" class="categories">
-          <strong>Categorias: </strong>
+          <strong>Categorías: </strong>
           <span v-for="genre in serie.genres" :key="genre.id" class="category">
             {{ genre.name }}
           </span>
@@ -24,16 +24,31 @@
         </span>
       </div>
 
-      <p><strong>Año: </strong> </p>
-
       <div v-if="serie.created_by.length" class="categories">
           <strong>Creador: </strong>
-          <span v-for="created_by in serie.created_by" :key="created_by.id" class="category">
-            {{ created_by.name }}
+          <span v-for="creator in serie.created_by" :key="creator.id" class="category">
+            {{ creator.name }}
           </span>
       </div>
-
     </div>
+
+    <div v-if="cast.length">
+      <h3>Reparto:</h3>
+      <div class="cast-list">
+        <div v-for="actor in cast" :key="actor.id" class="cast-item">
+          <img
+            v-if="actor.profile_path"
+            :src="`https://image.tmdb.org/t/p/w200${actor.profile_path}`"
+            :alt="actor.name"
+            class="cast-photo"
+          />
+          <p>
+            <strong>{{ actor.name }}</strong> como: {{ actor.roles[0]?.character || 'Desconocido' }}
+          </p>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -46,24 +61,32 @@ export default {
       serie: {
         genres: [],
         created_by: [],
-        keywords: [],
-        category: [] 
-      }
+        keywords: [] // Almacenamos aquí las palabras clave
+      },
+      cast: [] // Almacenamos aquí el reparto (cast)
     }
   },
   async mounted() {
     const serieId = this.$route.params.id
 
     try {
+      // Obtienes los detalles de la serie
       const response = await axios.get(`https://api.themoviedb.org/3/tv/${serieId}`, {
         params: { api_key: 'b27d7edb3072175fb8681650517059f7' }
       })
       this.serie = response.data
 
+      // Obtienes las palabras clave (keywords) de la serie
       const keywordsResponse = await axios.get(`https://api.themoviedb.org/3/tv/${serieId}/keywords`, {
         params: { api_key: 'b27d7edb3072175fb8681650517059f7' }
       })
       this.serie.keywords = keywordsResponse.data.results || [] 
+
+      // Obtienes el reparto (cast) de la serie
+      const castResponse = await axios.get(`https://api.themoviedb.org/3/tv/${serieId}/aggregate_credits`, {
+        params: { api_key: 'b27d7edb3072175fb8681650517059f7' }
+      })
+      this.cast = castResponse.data.cast || []
 
     } catch (error) {
       console.error('Fallo en obtener información de la serie:', error)
