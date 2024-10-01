b0aba79f6b79ab74c6ca1a9379f5d23bc90dd7bd
ramsesar1 <90067815+ramsesar1@users.noreply.github.com>
2024-10-01 00:46:18 -0700

    se añade navbar en moviesapi

--- zumacinevue/src/components/MoviesAPI.vue
+++ zumacinevue/src/components/MoviesAPI.vue
@@ -1,41 +1,52 @@
 <template>
+  <Navbar />
   <div>
     <h2>Movies</h2>
     <div v-if="error" class="error">{{ error }}</div>
     <div v-else-if="loading" class="loading">Loading...</div>
     <div v-else class="movies-container">
       <div class="card" v-for="movie in movies" :key="movie.id">
-        <img v-if="movie.poster_path" :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="movie.title"
-          class="movie-poster" />
+        <img
+          v-if="movie.poster_path"
+          :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
+          :alt="movie.title"
+          class="movie-poster"
+        />
         <div class="card-content">
           <h3>{{ movie.title }}</h3>
           <p class="overview">{{ movie.overview }}</p>
           <router-link :to="`/movie/${movie.id}`" class="details-link">Ver Detalles</router-link>
         </div>
       </div>
-      <button @click="loadMoreMovies" v-if="!loadingMore" class="load-more-btn">Cargar más películas</button>
+      <button @click="loadMoreMovies" v-if="!loadingMore" class="load-more-btn">
+        Cargar más películas
+      </button>
       <div v-if="loadingMore" class="loading-more">Cargando más películas...</div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import Navbar from '@/components/Navbar.vue'
+import axios from 'axios'
 
 export default {
   name: 'MoviesAPI',
+  components: {
+    Navbar
+  },
   data() {
     return {
       movies: [],
       loading: true,
       loadingMore: false,
       error: null,
-      currentPage: 1,
-    };
+      currentPage: 1
+    }
   },
 
   async mounted() {
-    await this.fetchMovies(this.currentPage);
+    await this.fetchMovies(this.currentPage)
   },
 
   methods: {
@@ -44,26 +55,26 @@ export default {
         const response = await axios.get('https://api.themoviedb.org/3/discover/movie', {
           params: {
             api_key: 'b27d7edb3072175fb8681650517059f7',
-            page: page,
-          },
-        });
-        this.movies.push(...response.data.results);
+            page: page
+          }
+        })
+        this.movies.push(...response.data.results)
       } catch (error) {
-        this.error = 'Fallo en cargar más películas, intente de nuevo';
-        console.error(error);
+        this.error = 'Fallo en cargar más películas, intente de nuevo'
+        console.error(error)
       } finally {
-        this.loading = false;
-        this.loadingMore = false;
+        this.loading = false
+        this.loadingMore = false
       }
     },
 
     async loadMoreMovies() {
-      this.loadingMore = true;
-      this.currentPage += 1;
-      await this.fetchMovies(this.currentPage);
-    },
-  },
-};
+      this.loadingMore = true
+      this.currentPage += 1
+      await this.fetchMovies(this.currentPage)
+    }
+  }
+}
 </script>
 
 <style scoped>
